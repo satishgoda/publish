bc75e47a3d05d72fffd0bcea0f459f9d84b82911
wangbin <852854910@qq.com>
2017-06-15 12:25:12 +0800

    add

--- shotguntest/shotguntest.py
+++ shotguntest/shotguntest.py
@@ -42,27 +42,90 @@ fields = ["code"]
 #print Project
 
 
-def creat_new_version(entity_name,task_name,version_name,project_name,description,uploaded_movie_path=None):
+def creat_Asst_new_version(asset_name,task_name,version_name,ple_step,project_name,description,uploaded_movie_path=None,local_path=None,status="ip"):
     filters = [ ['name', 'is',project_name]]
     Project = sg.find_one('Project', filters)
+    filters = [ ['project', 'is',Project],
+                 ['code', 'is', asset_name] ]
+    asset = sg.find_one('Asset', filters)
+    task=sg.find("Task",[ ['project', 'is',Project],["content","is",task_name],["step",'name_is',ple_step],['entity','is',asset]])
     data = { 'project': Project,
-             'code': 'main.v008',
-             'description': u'这是个测试',
-             'sg_status_list': 'rev',
+             'code': version_name,
+             'description': description,
+             'sg_status_list': status,
              'entity': asset,
-             'sg_task': task}
-    if uploaded_movie_path:
-        data = {'sg_uploaded_movie': {'local_path': uploaded_movie_path,
-    #                               'name': 'Better Movie'}}
+             'sg_task': task[0]}
+    Version = sg.create('Version', data)
+    data = {'sg_localpath':{'local_path': local_path}}
+    result = sg.upload("Version",Version['id'],uploaded_movie_path,field_name="sg_uploaded_movie")
+    result = sg.update("Version",Version['id'],data)
 
+### Asst_new_version
+# asset_name="xiaohei"
+# task_name="default"
+# version_name="main.v011"
+# project_name="LST"
+# description="ceshi yige "
+# ple_step="cfx"
+# uploaded_movie_path="D:\\pletest\\shotgun\\projects\\lst\\assets\\Character\\xiaobai\\model\\pub\\v001\\main.mov"
+# local_path="D:\\pletest\\shotgun\\projects\\lst\\assets\\Character\\xiaobai\\model\\pub\\v001\\"
+# status="ip"
+# creat_Asst_new_version(asset_name,task_name,version_name,ple_step,project_name,description,uploaded_movie_path,local_path,status)
+###
+
+
+
+def set_task_status(project_name,asset_name,task_name,ple_step,task_ststus,version_ststus):
+    filters = [['name', 'is', project_name]]
+    Project = sg.find_one('Project', filters)
+    filters = [['project', 'is', Project],
+               ['code', 'is', asset_name]]
+    asset = sg.find_one('Asset', filters)
+
+    task=sg.find("Task",[ ['project', 'is',Project],["content","is",task_name],["step",'name_is',ple_step],['entity','is',asset]])
+    Last_version=get_laset_version(task)
+    task_data = {'sg_status_list':task_ststus}
+    version_data = {'sg_status_list':version_ststus}
+    result = sg.update("Version",Last_version['id'],version_data)
+    result = sg.update("Task",task[0]['id'],task_data)
+
+
+def get_laset_version(task):
+    Version=sg.find("Version",[["sg_task","is",task]],["created_at"])
+    all_time=[x["created_at"] for x in  Version]
+    all_time.sort()
+    for x in Version:
+        if x["created_at"]==all_time[-1]:
+            return x
+
+### work set_task_status
+# asset_name="xiaohei"
+# project_name="LST"
+# task_name="default"
+# ple_step="cfx"
+# task_ststus="final"
+# version_ststus="down"
+# set_task_status(project_name,asset_name,task_name,ple_step,task_ststus,version_ststus)
+###
+
+
+
+
+
+
+
+#print Last_version
+
+# sg_task=
+# created_at
+#def
 #Version = sg.create('Version', data)
 #print Version
 
 #Versionid=6751
 
-# data = {'sg_uploaded_movie': {'local_path': 'D:/pletest/shotgun/funcs/main.mov',
-#                               'name': 'Better Movie'}}
-#result = sg.update("Version",6746,data)
+
+#result = sg.update("Version",6768,data)
 
 
 # upload .mov
